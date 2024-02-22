79c90b548698fe53565715469bb4412089166099
vishnu nair <infovishnunair@gmail.com>
2024-02-22 22:26:58 +0530

    Update TopNavbar.jsx

--- src/Pages/homePage/TopNavbar/TopNavbar.jsx
+++ src/Pages/homePage/TopNavbar/TopNavbar.jsx
@@ -45,40 +45,23 @@ function TopNavbar() {
         <li className="nav-item">
           <div className="nav-link" onClick={handlemybooking}>My Booking</div>
         </li>
-        {/* <li className="nav-item">
-          <div className="nav-link" href="#">Link</div>
-        </li> */}
-        {/* <li className="nav-item">
-          <div className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</div>
-        </li> */}
+        
       </ul>
       <div className='rightsidetopnav'>
 
-      {/* <form className="d-flex"> */}
-        {/* <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/> */}
-        {/* <button className="btn btn-outline-primary" type="submit">Search</button> */}
-        {/* <button className="btn btn-primary" type="submit">Search</button> */}
-      {/* </form> */}
 
       <li class="nav-item dropdown d-flux">
           <span className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            USER
+            <img src={userLogo} alt="" />
           </span>
           <ul className="dropdown-menu " aria-labelledby="navbarDropdown">
             <li><span className="dropdown-item" >Name:{userDetails.firstName}{userDetails.lastName}</span></li>
             <li><span className="dropdown-item" >user: {userDetails.role === 1 ? 'Admin' : 'User'}</span></li>
 
             <div className='text-center'><button className="btn btn-primary " onClick={handlelogout}>Log Out</button></div>
-            {/* <li><hr class="dropdown-divider"></li> */}
-            {/* <li><a class="dropdown-item" href="#">Something else here</a></li> */}
           </ul>
         </li>
-        
 
-      
-       {/* <div>{userDetails.firstName}{userDetails.lastName}</div>
-      <img src={userLogo} alt="" />  */}
-      
       </div>
       
 
@@ -92,64 +75,3 @@ function TopNavbar() {
 
 export default TopNavbar
 
-// import React from 'react';
-
-// function TopNavbar() {
-//   return (
-//     <div>
-//       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//         <div className="container-fluid">
-//           <a className="navbar-brand" href="#">Navbar</a>
-//           <button
-//             className="navbar-toggler"
-//             type="button"
-//             data-bs-toggle="collapse"
-//             data-bs-target="#navbarTogglerDemo02"
-//             aria-controls="navbarTogglerDemo02"
-//             aria-expanded="false"
-//             aria-label="Toggle navigation"
-//           >
-//             <span className="navbar-toggler-icon"></span>
-//           </button>
-//           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-//             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-//               <li className="nav-item">
-//                 <a className="nav-link active" aria-current="page" href="#">
-//                   Home
-//                 </a>
-//               </li>
-//               <li className="nav-item">
-//                 <a className="nav-link" href="#">
-//                   Link
-//                 </a>
-//               </li>
-//               <li className="nav-item">
-//                 <a
-//                   className="nav-link disabled"
-//                   href="#"
-//                   tabIndex="-1"
-//                   aria-disabled="true"
-//                 >
-//                   Disabled
-//                 </a>
-//               </li>
-//             </ul>
-//             <form className="d-flex">
-//               <input
-//                 className="form-control me-2"
-//                 type="search"
-//                 placeholder="Search"
-//                 aria-label="Search"
-//               />
-//               <button className="btn btn-outline-success" type="submit">
-//                 Search
-//               </button>
-//             </form>
-//           </div>
-//         </div>
-//       </nav>
-//     </div>
-//   );
-// }
-
-// export default TopNavbar;
