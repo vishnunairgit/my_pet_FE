74adf2d78cd4a88e0ab2802607207aea73767988
Nair <154236730+vishnunairgit@users.noreply.github.com>
2024-03-18 10:28:04 +0530

    18-03-2024

--- src/Pages/petGrooming/PetGrooming.jsx
+++ src/Pages/petGrooming/PetGrooming.jsx
@@ -1,283 +1,225 @@
 
-import React, { useState } from 'react'
+
+import React, { useState } from 'react';
 import AxiosInstance from '../../config/AxiosInstance';
 
 function PetGrooming() {
-
     const [addgrooming, setaddgrooming] = useState({
-        petgroomingpackage:'',
-        GeoomingSesssion:'',
-        ServiceCharge:'',
-        Duration:'',
-    })
-    
+        petgroomingpackage: '',
+        GroomingSesssion: [], // Change to an array to hold multiple sessions
+        ServiceCharge: '',
+        Duration: '',
+    });
 
-  // -----------grooming package data--------------
+    const [GroomingSesssion, setGroomingSesssion] = useState([])
 
-  const [GroomingSesssion, setGroomingSesssion] = useState([
-    'Bath',
-    'Teeth Brushing',
-    'Ear Cleaning',
-    'Nail Cutting',
-    'Eye Cleaning',
-    'Anal sac cleaning',
-    'Coat Conditioning',
-    'Hygiene Trim',
-    'De-Matting',
-  ])
-
-  // -----------service charge and duration -------------
-  const [Charge, setCharge] = useState('')
-  const [Duration, setDuration] = useState('')
-
-
-
-  const handlegroomingSection = (e, index) => {
-
-    const updatePoints = [...GroomingSesssion];
-    updatePoints[index] = e.target.value;
-    setGroomingSesssion(updatePoints);
-  }
-
-// -------servicce charge------------
-
-const handlePackages = (e) => {
-    const selectedpackages = e.target.value;
-    setCharge(selectedpackages);
-    setDuration(selectedpackages);
+    const handlePackages = (e) => {
+      const selectedPackage = e.target.value;
+      let charge = '';
+      let duration = '';
+      let GroomingSessions = [];
   
-    if (selectedpackages === 'NORMAL') {
-      setCharge('2000.00');
-      setDuration('01 / 3 month');
-    }
-    else if (selectedpackages === 'PRIME') {
-      setCharge('3500.00');
-      setDuration('02 / 5 month');
-    }
-    else if (selectedpackages === 'PRIME GOLD'){
-      setCharge('8000.00');
-      setDuration('05 / 8 month');
-    }
-    else if (selectedpackages === 'PRIME DIAMOND' ) {
-      setCharge('15000.00');
-      setDuration('10 / 1 year');
-    }
-    else {
-      setCharge('');
-      setDuration('');
-    }
+      if (selectedPackage === 'NORMAL') {
+          GroomingSessions = [
+              'Bath',
+              'Teeth Brushing',
+              'Ear Cleaning',
+              'Nail Cutting',
+              'Eye Cleaning',
+              'Anal sac cleaning',
+              'Coat Conditioning',
+              'Hygiene Trim',
+              'De-Matting'
+          ];
+          charge = '2000.00';
+          duration = '01 / 3 month';
+      } else if (selectedPackage === 'PRIME') {
+          GroomingSessions = [
+              'Bath',
+              'Teeth Brushing',
+              'Ear Cleaning',
+              'Nail Cutting'
+          ];
+          charge = '3500.00';
+          duration = '02 / 5 month';
+      } else if (selectedPackage === 'PRIME GOLD') {
+          GroomingSessions = [
+              'Bath',
+              'Teeth Brushing',
+              'Ear Cleaning',
+              'Nail Cutting',
+              'Eye Cleaning',
+              'Anal sac cleaning',
+              'Coat Conditioning',
+              'Hygiene Trim'
+          ];
+          charge = '8000.00';
+          duration = '05 / 8 month';
+      } else if (selectedPackage === 'PRIME DIAMOND') {
+          GroomingSessions = [
+              'Bath',
+              'Teeth Brushing',
+              'Ear Cleaning',
+              'Nail Cutting',
+              'Eye Cleaning',
+              'Anal sac cleaning',
+              'Coat Conditioning',
+              'Hygiene Trim',
+              'De-Matting'
+          ];
+          charge = '15000.00';
+          duration = '10 / 1 year';
+      }
   
-    console.log("Selected Package:", selectedpackages);
-    console.log("Charge:", Charge);
-    console.log("Duration:", Duration);
-  }
-
-  const handlegrooming = (e)=>{
-    setaddgrooming({...addgrooming, [e.target.name]: e.target.value})
-
-
-
-  }
-
-  const handleSubmitpetGrooming =()=>{
-    AxiosInstance.post('/admin/addGrooming', addgrooming)
-    .then((responce)=>{
-        debugger
-        console.log(responce);
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
-  }
+      setaddgrooming({
+          ...addgrooming,
+          petgroomingpackage: selectedPackage,
+          ServiceCharge: charge,
+          Duration: duration,
+          GroomingSesssion: GroomingSessions
+      });
+  };
   
-  return (
-    <div>
-      <div className="addtree">
-        {/* <form> */}
-        {/* <form onSubmit={handleSubmitAddPet}> */}
+    const handlegroomingSection = (e, index) => {
+      setaddgrooming(prevState => {
+          const updatedSessions = [...prevState.GroomingSesssion];
+          updatedSessions[index] = e.target.value;
+          return { ...prevState, GroomingSesssion: updatedSessions };
+      });
+  };
+  
 
-        <div className="container">
-          <div className="leftSide-container">
-            <h2>GROOMING</h2>
-            <div className="Tree-thumbnail">
-              <div className="Tree-thumbnail-leftside">
-              
+    const handlegrooming = (e) => {
+        setaddgrooming({ ...addgrooming, [e.target.name]: e.target.value });
+    };
 
-              </div>
-             
+    const handleSubmitpetGrooming = () => {
+        AxiosInstance.post('/admin/addGrooming', addgrooming)
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
+    return (
+        <div>
+            <div className="addtree">
+                <div className="container">
+                    <div className="leftSide-container">
+                        <h2>GROOMING</h2>
+
+                        <div className="row">
+                            <div className="col-25">
+                                <label htmlFor="petgroomingpackage">
+                                    PACKAGES <span className="mandatory-indicator">*</span>
+                                </label>
+                            </div>
+                            <div className="col-75">
+                                <select
+                                    value={addgrooming.petgroomingpackage}
+                                    id="petgroomingpackage"
+                                    name="petgroomingpackage"
+                                    onChange={(e) => {
+                                        handlePackages(e);
+                                    }}
+                                    required
+                                >
+                                    <option value="">Select Packages</option>
+                                    <option value="NORMAL">NORMAL</option>
+                                    <option value="PRIME">PRIME</option>
+                                    <option value="PRIME GOLD">PRIME GOLD</option>
+                                    <option value="PRIME DIAMOND">PRIME DIAMOND</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-25">
+                                <label htmlFor="GeoomingSesssion">
+                                    Grooming Session<span className="mandatory-indicator">*</span>
+                                </label>
+                            </div>
+                            <div className="col-75">
+                                {addgrooming.GroomingSesssion.map((point, index) => (
+                                    <div key={index} >
+                                        <input
+                                            type="text"
+                                            value={point}
+                                            // value={addgrooming.GroomingSesssion}
+                                            name={GroomingSesssion}
+                                            onChange={(e) => {
+                                                handlegroomingSection(e, index);
+                                            }}
+                                            required
+                                        />
+                                    </div>
+                                ))} 
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-25">
+                                <label htmlFor="ServiceCharge">
+                                    Service Charge<span className="mandatory-indicator">*</span>
+                                </label>
+                            </div>
+                            <div className="col-75">
+                                <input
+                                    value={addgrooming.ServiceCharge}
+                                    type="number"
+                                    id="ServiceCharge"
+                                    name="ServiceCharge"
+                                    placeholder="Service Charge.."
+                                    onChange={(e) => {
+                                        handlegrooming(e);
+                                    }}
+                                />
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-25">
+                                <label htmlFor="Duration">
+                                    Package Duration/ NO of grooming<span className="mandatory-indicator">*</span>
+                                </label>
+                            </div>
+                            <div className="col-75">
+                                <input
+                                    value={addgrooming.Duration}
+                                    type="text"
+                                    id="duration"
+                                    name="Duration"
+                                    placeholder="Duration..."
+                                    onChange={(e) => {
+                                        handlegrooming(e);
+                                    }}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="buttonHolder">
+                    <button
+                        className="button-17"
+                        type="button"
+                        style={{ backgroundColor: 'rgb(10, 150, 250)', color: 'white' }}
+                        onClick={handleSubmitpetGrooming}
+                    >
+                        Submit
+                    </button>
+                    <button className="button-17" style={{ backgroundColor: 'rgb(0, 150, 0)', color: 'white' }}>
+                        Reset
+                    </button>
+                    <button className="button-17" style={{ backgroundColor: 'rgb(200, 0, 0)', color: 'white' }}>
+                        Cancel
+                    </button>
+                </div>
             </div>
-
-
-            <div className="row">
-              <div className="col-25">
-                <label htmlFor="petgroomingpackage">
-                  PACKAGES <span className="mandatory-indicator">*</span>
-                </label>
-              </div>
-              <div className="col-75">
-                <select
-                value={addgrooming.petgroomingpackage}
-                  //   value={addPetForm.petGender}
-                  id="petgroomingpackage"
-                  name="petgroomingpackage"
-                  onChange={(e)=>{
-                    handlePackages(e);
-                    handlegrooming(e);
-                  }}
-
-                  required
-
-                >
-                  <option value="">Select Packages </option>
-                  <option value="NORMAL">NORMAL</option>
-                  <option value="PRIME">PRIME</option>
-                  <option value="PRIME GOLD">PRIME GOLD</option>
-                  <option value="PRIME DIAMOND">PRIME DIAMOND</option>
-                </select>
-              </div>
-            </div>
-
-
-            {/* <div className="row">
-              <div className="col-25">
-                <label htmlFor="GeoomingSesssion">
-                  Grooming Session<span className="mandatory-indicator">*</span>
-                </label>
-              </div>
-              <div className="col-75">
-                {GroomingSesssion.map((point, index) => (
-                  <div key={index}>
-                    <input
-                      type="text"
-                      value={point}
-                      name='GroomingSesssion'
-                      onChange={(e) => {
-                        handlegroomingSection(e, index)
-                        handlegrooming(e);
-                    }
-
-                      }
-                      required
-                    />
-                  </div>
-                ))}
-              </div>
-            </div>
-             */}
-             <div className="row">
-  <div className="col-25">
-    <label htmlFor="GeoomingSesssion">
-      Grooming Session<span className="mandatory-indicator">*</span>
-    </label>
-  </div>
-  <div className="col-75">
-    {GroomingSesssion.map((point, index) => (
-      <div key={index}>
-        <input
-          type="text"
-          value={point}
-          name={`GroomingSesssion-${index}`} // Set a unique name for each input
-          onChange={(e) => {
-            handlegroomingSection(e, index);
-            handlegrooming(e);
-          }}
-          required
-        />
-      </div>
-    ))}
-  </div>
-</div>
-
-
-            <div className="row">
-              <div className="col-25">
-                <label htmlFor="ServiceCharge">
-                  Service Charge<span className="mandatory-indicator">*</span>
-                </label>
-              </div>
-              <div className="col-75">
-                <input
-                  value={Charge}
-                  type="number"
-                  id="ServiceCharge"
-                  name="ServiceCharge"
-                  placeholder="Service Charge.."
-                  onChange={(e)=> {
-                    setCharge(e.target.value) // If you want to allow manual input as well
-                    handlegrooming(e);
-
-
-                }}
-                />
-              </div>
-            </div>
-
-            <div className="row">
-              <div className="col-25">
-                <label htmlFor="Duration">
-                  Package Duration/ NO of grooming<span className="mandatory-indicator">*</span>
-                </label>
-              </div>
-              <div className="col-75">
-                <input
-                  value={Duration}
-                  type="text"
-                  id="duration "
-                  name="duration"
-                  placeholder="Duration..."
-                  onChange={(e)=> {
-                    setDuration(e.target.value)// If you want to allow manual input as well
-
-                     handlegrooming(e);
-
-                }} 
-                //   onChange={onChange}
-                />
-              </div>
-            </div>
-
-
-
-
-
-          </div>
-
-
         </div>
-
-        {/* {error && <ErrorMessage message={error} />} */}
-        <div className="buttonHolder">
-          <button
-            className="button-17"
-            type="submit"
-            style={{ backgroundColor: "rgb(10, 150, 250)", color: "white" }}
-          onClick={handleSubmitpetGrooming}
-          >
-            Submit
-          </button>
-          <button
-            className="button-17"
-            // onClick={handleReset}
-            style={{ backgroundColor: "rgb(0, 150, 0)", color: "white" }}>
-            Reset
-          </button>
-          <button
-            className="button-17"
-            // onClick={handleCancel}
-            style={{ backgroundColor: "rgb(200, 0, 0)", color: "white" }}>
-            Cancel
-          </button>
-        </div>
-
-        {/* </form> */}
-      </div>
-
-
-
-
-
-    </div>
-  )
+    );
 }
 
-export default PetGrooming
+export default PetGrooming;
